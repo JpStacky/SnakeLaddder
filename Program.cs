7809be2ecaf8cea7d46b898627a9af291dc13266
Jp <jyotiprakashpc@outlook.com>
2022-08-02 13:20:08 +0530

    Multipalyer mode

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake Ladder Game");
+            MultiPlayerUC7.setPosition();
         }
     }
 }
