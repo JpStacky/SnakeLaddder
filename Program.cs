d023f164ddff95d17d440b0eaddbc58c0da68b9d
Jp <jyotiprakashpc@outlook.com>
2022-08-02 13:07:24 +0530

    Updated Program

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake Ladder Game");
+            SetPositionUC1.setPosition();
         }
     }
 }
