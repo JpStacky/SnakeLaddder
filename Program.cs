6064eb24c36dda2ab213a8acfcc98b546093c4a8
Jp <jyotiprakashpc@outlook.com>
2022-08-02 13:18:33 +0530

    No of times the die rolled

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake Ladder Game");
+            TimesDieRolledUC6.setPosition();
         }
     }
 }
