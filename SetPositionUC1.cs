ad20d0ca0b06054373dd24027dc4c1bec917800c
Jp <jyotiprakashpc@outlook.com>
2022-08-02 13:06:08 +0530

    Set Initial Position

---
+++ SetPositionUC1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class SetPositionUC1
+    {
+        public static int initialposition = 0;
+        public static void setPosition()
+        {
+            Console.WriteLine("Starting game in Single Player Mode");
+            Console.WriteLine("Starting at position "+initialposition);
+        }
+    }
+}
