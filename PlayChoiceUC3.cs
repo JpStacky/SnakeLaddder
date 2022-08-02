b365a19da141ee8763e242c4dcbd32eb25e4704a
Jp <jyotiprakashpc@outlook.com>
2022-08-02 13:12:53 +0530

    Play or No Play

---
+++ PlayChoiceUC3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class PlayChoiceUC3
+    {
+        static int DiceNum;
+        public static int position = 0;
+        public static void setPosition()
+        {
+            Console.WriteLine("Starting game in Single Player Mode");
+            Console.WriteLine("Starting at position " + position);
+            rollsDie();
+        }
+
+        public static void rollsDie()
+        {
+            Random random = new Random();
+            int choice = random.Next(3);
+            if (choice == 0)
+            {
+                Console.WriteLine("No PLay");
+                Console.WriteLine("Initial Position is " +position);
+            }
+            else if (choice == 1)
+            {
+                Console.WriteLine("Ladder is found");
+                int DiceNum = random.Next(6);
+                position += DiceNum;
+                Console.WriteLine("Initial Position is " + position);
+            }
+            else
+            {
+                Console.WriteLine("Snake is found");
+                int DiceNum = random.Next(6);
+                position -= DiceNum;
+                Console.WriteLine("Initial Position is " + position);
+            }
+        }
+    }
+}
