33aa5ab05d3912096e4f12106fe310b57f064373
Jp <jyotiprakashpc@outlook.com>
2022-08-02 13:10:32 +0530

    Roll Die

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake Ladder Game");
+            RollsDiceUC2.setPosition();
         }
     }
 }
---
+++ RollsDiceUC2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class RollsDiceUC2
+    {
+        public static int position = 0;
+        public static void setPosition()
+        {
+            Console.WriteLine("Starting game in Single Player Mode");
+            Console.WriteLine("Starting at position "+position);
+            rollDie();
+        }
+        public static void rollDie()              //num - receives the rolling number limit
+        {
+            Random random = new Random();
+            int DiceNum = random.Next(1,7);
+            Console.WriteLine("Dice Number " +DiceNum);
+            position += DiceNum;
+            Console.WriteLine("Current Position is "+position);
+        }
+    }
+}
